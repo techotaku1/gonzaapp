ff4c8f247e4062e773d1388ddcc3b69fe64ce071
techotaku1 <jsdg1818@gmail.com>
2025-06-10 22:00:10 -0500

    auto cierre

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { SignedIn } from '@clerk/nextjs';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 import { queryClient } from '~/lib/react-query';
+
+import ActivityMonitor from './ActivityMonitor';
 import '~/styles/background.css';
 
 export default function ClientLayout({
@@ -12,6 +15,9 @@ export default function ClientLayout({
 }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <SignedIn>
+        <ActivityMonitor />
+      </SignedIn>
       <div className="animated-background" />
       {children}
     </QueryClientProvider>
