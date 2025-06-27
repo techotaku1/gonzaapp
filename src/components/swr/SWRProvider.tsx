dd9626efe0d619ce666ccfa674ced6750fb57a3e
techotaku1 <jsdg1818@gmail.com>
2025-06-27 04:43:10 -0500

    swr full 1

--- src/components/swr/SWRProvider.tsx
+++ src/components/swr/SWRProvider.tsx
@@ -5,6 +5,9 @@ import { SWRConfig } from 'swr';
 const swrConfig = {
   revalidateOnFocus: true,
   revalidateOnReconnect: true,
+  refreshInterval: 2000, // Polling cada 2 segundos
+  refreshWhenHidden: true, // Polling aunque la pestaña esté oculta
+  refreshWhenOffline: true, // Polling cuando vuelva el internet
 };
 
 export function SWRProvider({ children }: { children: React.ReactNode }) {
