a53ad6f3ed7f16429ab1e42b17023aa53a957c7e
techotaku1 <jsdg1818@gmail.com>
2025-07-09 05:00:22 -0500

    api

--- src/env.ts
+++ src/env.ts
@@ -8,7 +8,6 @@ export const env = createEnv({
       .enum(['development', 'test', 'production'])
       .default('development'),
     CLERK_SECRET_KEY: z.string(),
-    NEON_API_KEY: z.string(),
   },
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
@@ -20,7 +19,6 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    NEON_API_KEY: process.env.NEON_API_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
